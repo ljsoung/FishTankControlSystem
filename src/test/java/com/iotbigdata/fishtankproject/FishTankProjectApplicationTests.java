6ad4e53ae758959a165f023dcd7b9bf850441cc4
ljsoung <super15600@gamil.com>
2025-10-15 11:49:37 +0900

    Initialize backend project

---
+++ src/test/java/com/iotbigdata/fishtankproject/FishTankProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iotbigdata.fishtankproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishTankProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
